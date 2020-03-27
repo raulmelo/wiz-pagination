3e1c7a9f988cef545c65b47526273a85a9aa6334
Hugo Ricchino <hugoricchino1@gmail.com>
2020-03-27 19:20:35 -0300

    finish logic

--- projects/wiz-pagination/src/lib/wiz-pagination.component.ts
+++ projects/wiz-pagination/src/lib/wiz-pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'wiz-pagination',
@@ -6,8 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wiz-pagination.component.css']
 })
 export class WizPaginationComponent implements OnInit {
-  public valueSelect: number;
-
+  @Input() valueSelect: number;
+  public filterItensMin: any;
+  public filterItensMax: any;
+  public minItem: number;
+  public maxItem: number;
+  public firstNumber: number;
+  public lastNumber: number;
+  
   public itens = [
     { 'value': 1 },
     { 'value': 2 },
@@ -16,19 +22,40 @@ export class WizPaginationComponent implements OnInit {
     { 'value': 5 },
     { 'value': 6 },
     { 'value': 7 },
-    { 'value': 8 },
-    { 'value': 9 },
   ]
 
   constructor() { }
 
   ngOnInit() {
 
+    this.select(this.valueSelect);
   }
 
   select(e) {
-    this.valueSelect = e.value;
+    if (this.itens.length != 0) {
+      this.firstNumber = this.itens[0].value;
+    }
+    console.log(this.itens.filter(x => x.value === e));
+    if (this.itens.filter(x => x.value === e).length !== 0) {
+      this.valueSelect = e;
+      this.filterItensMin = this.itens.filter(x => x.value < e);
+      this.filterItensMax = this.itens.filter(x => x.value > e);
 
+      if (this.filterItensMin.length !== 0) {
+        this.minItem = this.filterItensMin.pop().value;
+      } else {
+        this.minItem = null;
+      }
+      if (this.filterItensMax.length !== 0) {
+        this.maxItem = this.filterItensMax[0].value;
+        this.lastNumber = this.filterItensMax.pop().value;
+      } else {
+        this.lastNumber = this.itens[this.itens.length - 1].value;
+      }
+
+
+      console.log(this.filterItensMin);
+    }
   }
 }
 
